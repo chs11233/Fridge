a09dc5ec2cf07ef1a5029ee6cea248dbe0b04112
Holiday <chs11233@gmail.com>
2022-04-04 21:45:00 +0900

    first commit

--- app/src/main/java/com/example/fridge2/MainActivity.kt
+++ app/src/main/java/com/example/fridge2/MainActivity.kt
@@ -1,17 +1,25 @@
 package com.example.fridge2
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.fridge2.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private var mBinding: ActivityMainBinding? = null
     private val binding get() = mBinding!!
     private var auth: FirebaseAuth? = null
+
+    lateinit var navigationView: NavigationView
+    lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +30,18 @@ class MainActivity : AppCompatActivity() {
         // 툴바 생성
         val toolbar: Toolbar = binding.toolbar
         setSupportActionBar(toolbar)
-        supportActionBar!!.setTitle("냉장고에 뭐가있니") // 툴바 제목 설정
+        supportActionBar?.setTitle("냉장고에 뭐가있니") // 툴바 제목 설정
+        supportActionBar?.setDisplayHomeAsUpEnabled(true) // 드로어를 꺼낼 홈 버튼 활성화
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.navi_menu) // 홈버튼 이미지 변경
+        supportActionBar?.setDisplayShowTitleEnabled(false) // 툴바에 타이틀 안보이게
+
+
+        //NavigationDrawer 생성
+        drawerLayout = binding.drawerLayout
+
+        //NavigationDrawer 화면의 이벤트 처리 생성
+        navigationView = binding.navView
+        navigationView.setNavigationItemSelectedListener(this) //navigation 리스너
 
 
         // OnNavigationItemSelectedListener를 통해 탭 아이템 선택 시 이벤트를 처리
@@ -50,6 +69,31 @@ class MainActivity : AppCompatActivity() {
                 true
             }
             selectedItemId = R.id.main
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item!!.itemId){
+            android.R.id.home->{
+                drawerLayout.openDrawer(GravityCompat.START)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.logout-> Toast.makeText(this,"logout 실행", Toast.LENGTH_SHORT).show()
+        }
+        return false
+    }
+
+    override fun onBackPressed() {
+        if(binding.drawerLayout.isDrawerOpen(GravityCompat.START)){
+            binding.drawerLayout.closeDrawers()
+        } else {
+            super.onBackPressed()
         }
     }
 }
